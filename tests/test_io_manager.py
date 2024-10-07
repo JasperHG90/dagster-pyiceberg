13b4577fd952f41828ebd867f001a08bcf77de61
Jasper Ginn <jasperginn@gmail.com>
2024-10-07 13:46:10 +0200

    chore: add test

--- tests/test_io_manager.py
+++ tests/test_io_manager.py
@@ -2,7 +2,13 @@ import datetime as dt
 
 import pyarrow as pa
 import pytest
-from dagster import AssetExecutionContext, DailyPartitionsDefinition, asset, materialize
+from dagster import (
+    AssetExecutionContext,
+    DailyPartitionsDefinition,
+    HourlyPartitionsDefinition,
+    asset,
+    materialize,
+)
 from pyiceberg.catalog.sql import SqlCatalog
 
 from dagster_pyiceberg import IcebergPyarrowIOManager, IcebergSqlCatalogConfig
@@ -46,6 +52,19 @@ def b_plus_one(b_df: pa.Table) -> pa.Table:
 
 @asset(
     key_prefix=["my_schema"],
+    partitions_def=HourlyPartitionsDefinition(start_date=dt.datetime(2022, 1, 1, 0)),
+    config_schema={"value": str},
+    metadata={"partition_expr": "partition"},
+)
+def hourly_partitioned(context: AssetExecutionContext) -> pa.Table:
+    partition = dt.datetime.strptime(context.partition_key, "%Y-%m-%d-%H:%M")
+    value = context.op_execution_context.op_config["value"]
+
+    return pa.Table.from_pydict({"partition": [partition], "value": [value], "b": [1]})
+
+
+@asset(
+    key_prefix=["my_schema"],
     partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"),
     config_schema={"value": str},
     metadata={"partition_expr": "partition"},
@@ -73,16 +92,57 @@ def test_iceberg_io_manager_with_assets(tmp_path, sql_catalog, io_manager):
         assert out_dt["a"].to_pylist() == [2, 3, 4]
 
 
-def test_iceberg_io_manager_with_time_partitioned_assets(
+def test_iceberg_io_manager_with_daily_partitioned_assets(
     tmp_path, sql_catalog, io_manager
 ):
     resource_defs = {"io_manager": io_manager}
 
-    materialize(
-        [daily_partitioned],
-        partition_key="2022-01-01",
-        resources=resource_defs,
-        run_config={
-            "ops": {"my_schema__daily_partitioned": {"config": {"value": "1"}}}
-        },
-    )
+    for date in ["2022-01-01", "2022-01-02", "2022-01-03"]:
+        res = materialize(
+            [daily_partitioned],
+            partition_key=date,
+            resources=resource_defs,
+            run_config={
+                "ops": {"my_schema__daily_partitioned": {"config": {"value": "1"}}}
+            },
+        )
+        assert res.success
+
+    table = sql_catalog.load_table("dagster.daily_partitioned")
+    assert len(table.spec().fields) == 1
+    assert table.spec().fields[0].name == "partition_day"
+
+    out_df = table.scan().to_arrow()
+    assert out_df["partition"].to_pylist() == [
+        dt.date(2022, 1, 3),
+        dt.date(2022, 1, 2),
+        dt.date(2022, 1, 1),
+    ]
+
+
+def test_iceberg_io_manager_with_hourly_partitioned_assets(
+    tmp_path, sql_catalog, io_manager
+):
+    resource_defs = {"io_manager": io_manager}
+
+    for date in ["2022-01-01-01:00", "2022-01-01-02:00", "2022-01-01-03:00"]:
+        res = materialize(
+            [hourly_partitioned],
+            partition_key=date,
+            resources=resource_defs,
+            run_config={
+                "ops": {"my_schema__hourly_partitioned": {"config": {"value": "1"}}}
+            },
+        )
+        assert res.success
+
+    table = sql_catalog.load_table("dagster.hourly_partitioned")
+    assert len(table.spec().fields) == 1
+    assert table.spec().fields[0].name == "partition_hour"
+
+    out_df = table.scan().to_arrow()
+    assert out_df["partition"].to_pylist() == [
+        dt.datetime(2022, 1, 1, 3, 0),
+        dt.datetime(2022, 1, 1, 2, 0),
+        dt.datetime(2022, 1, 1, 1, 0),
+    ]
