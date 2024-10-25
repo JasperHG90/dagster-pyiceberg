c4d5dbdb6f3fda68413d404efa1c1c4ebf7c3427
Jasper Ginn <jasperginn@gmail.com>
2024-10-25 13:36:55 +0200

    chore: update load fn

--- packages/dagster_pyiceberg_polars/src/dagster_pyiceberg_polars/pyiceberg_polars_type_handler.py
+++ packages/dagster_pyiceberg_polars/src/dagster_pyiceberg_polars/pyiceberg_polars_type_handler.py
@@ -1,28 +1,38 @@
-from typing import Optional, Sequence, Type
+from typing import Optional, Sequence, Type, Union
 
 import polars as pl
 import pyarrow as pa
 from dagster._core.storage.db_io_manager import DbTypeHandler
+from dagster_pyiceberg import IcebergIOManager
 from dagster_pyiceberg.handler import (
     IcebergBaseArrowTypeHandler,
     IcebergPyArrowTypeHandler,
 )
-from dagster_pyiceberg.io_manager import IcebergIOManager
 from pyiceberg import table
 
+PolarsTypes = Union[pl.DataFrame, pl.LazyFrame]
 
-class IcebergPolarsTypeHandler(IcebergBaseArrowTypeHandler[pl.DataFrame]):
+
+class IcebergPolarsTypeHandler(IcebergBaseArrowTypeHandler[PolarsTypes]):
     def from_arrow(
-        self, obj: table.DataScan, target_type: Type[pl.DataFrame]
+        self, obj: table.DataScan, target_type: Type[PolarsTypes]
     ) -> pl.DataFrame:
-        return obj.to_polars()
+        return (
+            pl.from_arrow(obj.to_arrow())
+            if target_type == pl.DataFrame
+            else pl.from_arrow(obj.to_arrow_batch_reader())
+        )
 
-    def to_arrow(self, obj: pl.DataFrame) -> pa.Table:
-        return pa.Table.from_polars(obj)
+    def to_arrow(self, obj: PolarsTypes) -> pa.Table:
+        return (
+            obj.collect().to_arrow()
+            if isinstance(obj, pl.LazyFrame)
+            else obj.to_arrow()
+        )
 
     @property
     def supported_types(self) -> Sequence[Type[object]]:
-        return [pl.DataFrame]
+        return [pl.DataFrame, pl.LazyFrame]
 
 
 class IcebergPolarsIOManager(IcebergIOManager):
