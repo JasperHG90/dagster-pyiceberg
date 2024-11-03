9b155403435d43175aae23a023395c55409e624d
Jasper Ginn <jasperginn@gmail.com>
2024-11-03 14:56:45 +0100

    chore: do not register dagster version

--- packages/dagster_pyiceberg/src/dagster_pyiceberg/__init__.py
+++ packages/dagster_pyiceberg/src/dagster_pyiceberg/__init__.py
@@ -1,6 +1,6 @@
 from typing import Sequence
 
-from dagster._core.libraries import DagsterLibraryRegistry
+# from dagster._core.libraries import DagsterLibraryRegistry
 from dagster._core.storage.db_io_manager import DbTypeHandler
 from dagster_pyiceberg.config import (
     IcebergRestCatalogConfig as IcebergRestCatalogConfig,
@@ -11,7 +11,7 @@ from dagster_pyiceberg.handler import (
 )
 from dagster_pyiceberg.io_manager import IcebergIOManager as IcebergIOManager
 from dagster_pyiceberg.resource import IcebergTableResource as IcebergTableResource
-from dagster_pyiceberg.version import __version__
+from dagster_pyiceberg.version import __version__ as __version__
 
 
 class IcebergPyarrowIOManager(IcebergIOManager):
@@ -89,4 +89,4 @@ class IcebergPyarrowIOManager(IcebergIOManager):
         return [IcebergPyArrowTypeHandler()]
 
 
-DagsterLibraryRegistry.register("dagster-pyiceberg", __version__)
+# DagsterLibraryRegistry.register("dagster-pyiceberg", __version__)
--- packages/dagster_pyiceberg_polars/src/dagster_pyiceberg_polars/__init__.py
+++ packages/dagster_pyiceberg_polars/src/dagster_pyiceberg_polars/__init__.py
@@ -1,10 +1,10 @@
-from dagster._core.libraries import DagsterLibraryRegistry
+# from dagster._core.libraries import DagsterLibraryRegistry
 from dagster_pyiceberg_polars.pyiceberg_polars_type_handler import (
     IcebergPolarsIOManager as IcebergPolarsIOManager,
 )
 from dagster_pyiceberg_polars.pyiceberg_polars_type_handler import (
     IcebergPolarsTypeHandler as IcebergPolarsTypeHandler,
 )
-from dagster_pyiceberg_polars.version import __version__
+from dagster_pyiceberg_polars.version import __version__ as __version__
 
-DagsterLibraryRegistry.register("dagster-pyiceberg-polars", __version__)
+# DagsterLibraryRegistry.register("dagster-pyiceberg-polars", __version__)
